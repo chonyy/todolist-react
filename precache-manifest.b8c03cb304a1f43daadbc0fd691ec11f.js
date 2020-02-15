e4a5066da3b5f228acd759e125b3014aeddef656
Tony Chou <tcheon8788@gmail.com>
2020-02-16 02:47:24 +0800

    Updates

---
+++ precache-manifest.b8c03cb304a1f43daadbc0fd691ec11f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852670a16218e9b535047b1494cc1565",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "feb1d0c258300f8bc986",
+    "url": "/todolist-react/static/js/2.b0f29187.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist-react/static/js/2.b0f29187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f07e54b9db308c7e5fb",
+    "url": "/todolist-react/static/js/main.1fe6ad31.chunk.js"
+  },
+  {
+    "revision": "6c76c3075fa6583a668b",
+    "url": "/todolist-react/static/js/runtime-main.6e9b8191.js"
+  }
+]);
